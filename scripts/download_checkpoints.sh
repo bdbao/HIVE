fb00eb6cfcc5236330eb2df1e0ca39fd185d9c07
Duy-Bao Bui <91304039+bdbao@users.noreply.github.com>
2023-10-16 13:51:40 +0700

    fix: move cpkt to checkpoints folder

--- scripts/download_checkpoints.sh
+++ scripts/download_checkpoints.sh
@@ -9,3 +9,5 @@ wget https://huggingface.co/stabilityai/stable-diffusion-2-1-base/resolve/main/v
 /google-cloud-sdk/bin/gcloud storage cp gs://sfr-hive-data-research/checkpoints/hive_v2_rw_condition.ckpt $SCRIPT_DIR/../checkpoints/hive_v2_rw_condition.ckpt
 /google-cloud-sdk/bin/gcloud storage cp gs://sfr-hive-data-research/checkpoints/hive_rw.ckpt $SCRIPT_DIR/../checkpoints/hive_rw.ckpt
 /google-cloud-sdk/bin/gcloud storage cp gs://sfr-hive-data-research/checkpoints/hive_v2_rw.ckpt $SCRIPT_DIR/../checkpoints/hive_v2_rw.ckpt
+
+#move ./v2-1_512-ema-pruned.ckpt to checkpoints/v2-1_512-ema-pruned.ckpt
