22e2e4a8be25bd063e48c3bb8fe9fe58c9007fdc
Duy-Bao Bui <91304039+bdbao@users.noreply.github.com>
2023-10-31 22:21:39 +0700

    chore: add ~/gcloud
    for `ssh root@selab`

--- scripts/download_checkpoints.sh
+++ scripts/download_checkpoints.sh
@@ -5,9 +5,11 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 mkdir -p $SCRIPT_DIR/../checkpoints
 
 wget https://huggingface.co/stabilityai/stable-diffusion-2-1-base/resolve/main/v2-1_512-ema-pruned.ckpt $SCRIPT_DIR/../checkpoints/v2-1_512-ema-pruned.ckpt
-/google-cloud-sdk/bin/gcloud storage cp gs://sfr-hive-data-research/checkpoints/hive_rw_condition.ckpt $SCRIPT_DIR/../checkpoints/hive_rw_condition.ckpt
-/google-cloud-sdk/bin/gcloud storage cp gs://sfr-hive-data-research/checkpoints/hive_v2_rw_condition.ckpt $SCRIPT_DIR/../checkpoints/hive_v2_rw_condition.ckpt
-/google-cloud-sdk/bin/gcloud storage cp gs://sfr-hive-data-research/checkpoints/hive_rw.ckpt $SCRIPT_DIR/../checkpoints/hive_rw.ckpt
-/google-cloud-sdk/bin/gcloud storage cp gs://sfr-hive-data-research/checkpoints/hive_v2_rw.ckpt $SCRIPT_DIR/../checkpoints/hive_v2_rw.ckpt
+mv v2-1_512-ema-pruned.ckpt $SCRIPT_DIR/../checkpoints
+
+~/google-cloud-sdk/bin/gcloud storage cp gs://sfr-hive-data-research/checkpoints/hive_rw_condition.ckpt $SCRIPT_DIR/../checkpoints/hive_rw_condition.ckpt
+~/google-cloud-sdk/bin/gcloud storage cp gs://sfr-hive-data-research/checkpoints/hive_v2_rw_condition.ckpt $SCRIPT_DIR/../checkpoints/hive_v2_rw_condition.ckpt
+~/google-cloud-sdk/bin/gcloud storage cp gs://sfr-hive-data-research/checkpoints/hive_rw.ckpt $SCRIPT_DIR/../checkpoints/hive_rw.ckpt
+~/google-cloud-sdk/bin/gcloud storage cp gs://sfr-hive-data-research/checkpoints/hive_v2_rw.ckpt $SCRIPT_DIR/../checkpoints/hive_v2_rw.ckpt
 
 #move ./v2-1_512-ema-pruned.ckpt to checkpoints/v2-1_512-ema-pruned.ckpt
