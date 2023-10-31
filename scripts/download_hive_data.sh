cf59e783817df2404c4a1433c43356819b194ea6
Duy-Bao Bui <91304039+bdbao@users.noreply.github.com>
2023-10-31 22:17:48 +0700

    chore: add ~/gcloud

--- scripts/download_hive_data.sh
+++ scripts/download_hive_data.sh
@@ -6,9 +6,9 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 mkdir -p $SCRIPT_DIR/../data
 
 # Download HIVE training data
-/google-cloud-sdk/bin/gsutil -m cp -r gs://sfr-hive-data-research/data/training $SCRIPT_DIR/../data/
+~/google-cloud-sdk/bin/gsutil -m cp -r gs://sfr-hive-data-research/data/training $SCRIPT_DIR/../data/
 
 # Download HIVE evaluation data
 
-/google-cloud-sdk/bin/gcloud storage cp gs://sfr-hive-data-research/data/test.jsonl $SCRIPT_DIR/../data/test.jsonl
-/google-cloud-sdk/bin/gsutil -m cp -r gs://sfr-hive-data-research/data/evaluation $SCRIPT_DIR/../data/
+~/google-cloud-sdk/bin/gcloud storage cp gs://sfr-hive-data-research/data/test.jsonl $SCRIPT_DIR/../data/test.jsonl
+~/google-cloud-sdk/bin/gsutil -m cp -r gs://sfr-hive-data-research/data/evaluation $SCRIPT_DIR/../data/
